f7a0fb9ff3482019a0350670124e77f541a59165
DK7602 <dking7602@gmail.com>
2026-03-05 21:54:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ let sheetHeaderBtnLock = false;
 
 /***********************
 FORCE-NUKE OLD SERVICE WORKER CACHE
-***********************/
-  
+***********************/ 
 try{
   if("serviceWorker" in navigator){
     navigator.serviceWorker.getRegistrations()
